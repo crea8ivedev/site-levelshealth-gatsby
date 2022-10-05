509e09957e50276d16dfa93af260803001db8e35
Creative Dev <crea8ivedev@gmail.com>
2022-10-05 19:34:17 +0530

    Phase 2: Design changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,7 +33,7 @@ const makePages = async ({ posts, gatsbyUtilities }, template = "page") => {
 async function getPosts({ graphql, reporter }) {
   const graphqlResult = await graphql(/* GraphQL */ `
     query WpPosts {
-      blogPosts: allWpPost(sort: {fields: date, order: DESC}, limit: 11) {
+      blogPosts: allWpPost(sort: {fields: date, order: DESC}) {
         edges {
           post: node {
             id
